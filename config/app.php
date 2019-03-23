bc29058cbadd010141c52ed0cf4465ee3a5249b4
shpaq23 <shpaq23@gmail.com>
2019-03-23 14:35:20 +0100

    CORS PROXY updated

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Barryvdh\Cors\ServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         /*
          * Application Service Providers...
